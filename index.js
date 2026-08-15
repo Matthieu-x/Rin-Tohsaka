aec2e93af8d01af1ea7eb06439789c182c273dd3
Edward <cololacalempira5@gmail.com>
2026-08-15 11:50:15 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -37,12 +37,12 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
 console.log(chalk.magentaBright('\n🌾 Iniciando...'))
-say('MichiWA', {
+say('RinTohsaka', {
 font: 'simple',
 align: 'left',
 gradient: ['yellow', 'white']
 })
-say('Made with Ado', {
+say('Made with Rin', {
 font: 'console',
 align: 'center',
 colors: ['red', 'magenta', 'yellow']
