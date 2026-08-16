e5ac655227f16ea2ef0c1511a631f8aac63a7df1
Edward <cololacalempira5@gmail.com>
2026-08-16 13:11:04 -0600

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -12,7 +12,7 @@ global.owner = [
   ["", "", true],
 
   // ZONA DE LIDS
-  ["77623648624677", "Duan Edward", true],
+  ["7762364862467", "Duan Edward", true],
   ["204148502954022", "BrayanRK", true],
   ["", "", true]
 ]
