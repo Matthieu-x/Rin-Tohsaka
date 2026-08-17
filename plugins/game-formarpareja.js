525365016cf2415768e3e6edb9da9af96a3c558a
yosue891 <yosueortega630@gmail.com>
2026-08-17 14:03:11 -0400

    Add pareja formation handler for group chats

---
+++ plugins/game-formarpareja.js
@@ -0,0 +1,30 @@
+const toM = (a) => '@' + a.split('@')[0]
+
+const handler = async (m, { conn, groupMetadata }) => {
+    const ps = groupMetadata.participants.map((v) => v.id)
+    const a = ps[Math.floor(Math.random() * ps.length)]
+    let b
+    do b = ps[Math.floor(Math.random() * ps.length)]
+    while (b === a)
+
+    await conn.sendMessage(
+        m.chat,
+        {
+            text:
+                `ꕥ *Pareja Formada*\n\n` +
+                `〄 *Integrantes*\n` +
+                `> ${toM(a)} y ${toM(b)}\n\n` +
+                `> Deberían casarse, hacen una bonita pareja`,
+            mentions: [a, b]
+        },
+        { quoted: m }
+    )
+}
+
+handler.help = ['formarpareja']
+handler.tags = ['fun']
+handler.command = ['formarpareja', 'formarparejas']
+handler.group = true
+handler.register = true
+
+export default handler
